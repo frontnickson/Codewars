0abacc9e3403f6a32c8706970ff08f467f7122a1
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-07-01 14:28:07 +0300

    modifed

---
+++ Task13/Task13.js
@@ -0,0 +1,18 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// https://www.codewars.com/kata/514a024011ea4fb54200004b/train/javascript
+
+// * url = "http://github.com/carbonfive/raygun" -> domain name = "github"
+// * url = "http://www.zombie-bites.com"         -> domain name = "zombie-bites"
+// * url = "https://www.cnet.com"                -> domain name = cnet"
+
+function domainName(url) {
+    return url
+        .replace("http://", "")
+        .replace("https://", "")
+        .replace("www.", "")
+        .split(".")[0]
+}
+
+const f = domainName("https://www.cnet.com")
+console.log(f)
